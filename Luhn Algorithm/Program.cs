cd2ff970245f91aa5c53dd0dd24e4b70f6cf20c0
Hashir Majeed <K034968@eltham-college.org.uk>
2020-09-09 11:30:44 +0100

    Add project files.

---
+++ Luhn Algorithm/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Luhn_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string card = "";
+            Console.WriteLine("Enter Credit Card Number: ");
+            card = Console.ReadLine();
+            Console.WriteLine(checkSum(card));
+        }
+
+        static bool checkSum(string creditCard)
+        {
+            bool checksum = false;
+            int total = 0;
+
+            for (int i = 0; i < creditCard.Length; i ++)
+            {
+                if (i % 2 == 0)
+                {
+                    total += calc(Convert.ToInt32(creditCard[i]));
+                    Console.WriteLine(calc(Convert.ToInt32(creditCard[i])));
+                }
+                else
+                {
+                    total += Convert.ToInt32(creditCard[i]);
+                    Console.WriteLine(Convert.ToInt32(creditCard[i]));
+                }
+            }
+            Console.WriteLine(total);
+            if (total % 10 == 0)
+            {
+                checksum = true;
+            }
+
+            return checksum;
+        }
+
+        static int calc(int num)
+        {
+            num = num * 2;
+            if (num > 9)
+            {
+                num -= 9;
+            }
+
+            return num;
+        }
+    }
+}
